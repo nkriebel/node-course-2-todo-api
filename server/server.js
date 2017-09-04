689b909925e05b50afc078c4bb50b3609fb630da
Noah Kriebel <nKriebel@Noahs-MacBook-Pro.local>
2017-09-04 12:08:07 -0400

    Testing

--- server/server.js
+++ server/server.js
@@ -1,50 +1,6 @@
-let mongoose = require('mongoose');
+const express = require('express');
 
-mongoose.promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// let Todo = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
-
-// let newTodo2 = new Todo({
-//     text: 'Run faster',
-//     completed: false
-// });
-
-// newTodo2.save().then((doc) => {
-//     console.log('Saved todo:' , doc);
-// }, (e) => {
-//     console.log('Unable to save todo');
-// });
-
-let User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-});
-
-let user1 = new User({
-    email: 'example@test.com'
-});
-user1.save().then((doc) => {
-    console.log('New user email:', doc);
-}, (e) => {
-    console.log('Unable to save data', e);
-});
